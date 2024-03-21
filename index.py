86aa4b3cdeae6f38f02972a0017975bb20437734
divpreet11 <divpreetkaur08@gmail.com>
2024-03-22 00:28:56 +0530

    initial message

--- index.py
+++ index.py
@@ -1,3 +1,16 @@
-a=5
-b=10
-print(a+b)
+def is_palindrome_extra_variable(num):
+    original_num = num
+    reversed_num = 0
+    
+    while num > 0:
+        digit = num % 10
+        reversed_num = (reversed_num * 10) + digit
+        num //= 10
+    
+    return original_num == reversed_num
+
+number = 12321
+if is_palindrome_extra_variable(number):
+    print(f"{number} is a palindrome.")
+else:
+    print(f"{number} is not a palindrome.")
